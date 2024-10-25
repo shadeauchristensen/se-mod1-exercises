0167e377f57125ea7dc39e8f8bc45cf76f857a09
Shadeau Christensen <Shadeau100@gmail.com>
2024-10-24 20:46:46 -0700

    Created class file for dragon, vampire and unicorn

---
+++ ruby_exercises/mythical-creatures/lib/vampire.rb
@@ -0,0 +1,20 @@
+class Vampire
+    attr_reader :name, :pet, :thirsty
+    def initialize(name, pet = 'bat')
+        @name = name
+        @pet = pet
+        @thirsty = true
+    end
+
+    def name
+        @name
+    end
+
+    def pet
+        @pet
+    end
+
+    def drink    
+        @thirsty = false
+    end
+end
